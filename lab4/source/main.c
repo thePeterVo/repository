a6a04dd206d6ed72b91dcb70a08c53a11ce61be6
Peter Vo <pvo007@wch136-22.cs.ucr.edu>
2020-04-14 21:56:50 -0700

    saving progress

--- lab4/source/main.c
+++ lab4/source/main.c
@@ -11,13 +11,75 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+#include "RIMS.h"
+
+enum States {start, state1, state2, state3, state4} state;
+void Tick {
+	switch(state) {
+		case Start:
+			state = state1;
+			break;
+		case state1:
+			if(PINA == 0x01) {
+				state = state2;
+			}
+			else {
+				state = state1;
+			}
+			break;
+		case state2:
+			if(PINA == 0x03) {
+				state = state4;
+			}
+			else if(PINA == 0x00) {
+				state = state1;
+			}
+			else {
+				state = state2;
+			}
+			break;
+		case state3:
+			if(PINA & 0x01) {
+				state = state4;
+			}
+			else {
+				state = state3;
+			}
+			break;
+		case state4:
+			if(~PINA & 0x01) {
+				state = state1;
+			}
+			else {
+				state = state4;
+			}
+			break;
+	}
+	switch(state) {
+		case start:
+			PORTC = 0x07;
+			break;
+		case state1:
+			PORTB = 0x01;
+			break;
+		case state2:
+			PORTB = 0x02;
+			break;
+		case state3:
+			PORTB = 0x02;
+			break;
+		case state4:
+			PORTB = 0x01;
+			break;
+	}
+}
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
-
-    /* Insert your solution below */
-    while (1) {
-
-    }
-    return 1;
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	state = Start;
+	while (1) {
+		Tick();
+    	}		
+    	return 1;
 }
